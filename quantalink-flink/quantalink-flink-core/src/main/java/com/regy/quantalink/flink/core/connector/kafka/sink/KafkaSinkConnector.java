fb9f3d22b4f2bac767ba3cf0298164d87195e1b9
ReGYChang <p714140432@gmail.com>
2023-11-07 18:00:59 +0800

    feat(kafka connector): added support for topic selector in Kafka sink connector for dynamic topic routing

--- quantalink-flink/quantalink-flink-core/src/main/java/com/regy/quantalink/flink/core/connector/kafka/sink/KafkaSinkConnector.java
+++ quantalink-flink/quantalink-flink-core/src/main/java/com/regy/quantalink/flink/core/connector/kafka/sink/KafkaSinkConnector.java
@@ -1,6 +1,7 @@
 package com.regy.quantalink.flink.core.connector.kafka.sink;
 
 import com.regy.quantalink.common.config.Configuration;
+import com.regy.quantalink.common.exception.ConfigurationException;
 import com.regy.quantalink.common.exception.ErrCode;
 import com.regy.quantalink.common.exception.FlinkException;
 import com.regy.quantalink.flink.core.connector.SinkConnector;
@@ -12,7 +13,6 @@ import io.vavr.control.Try;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchemaBuilder;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.sink.KafkaSinkBuilder;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -30,27 +30,35 @@ public class KafkaSinkConnector<T> extends SinkConnector<T, T> {
     public KafkaSinkConnector(StreamExecutionEnvironment env, Configuration config) {
         super(env, config);
         this.bootStrapServers = config.getNotNull(KafkaOptions.BOOTSTRAP_SERVERS, String.format("Kafka sink connector '%s' bootstrap servers must not be null", getName()));
-        this.topic = config.getNotNull(KafkaOptions.TOPICS, String.format("Kafka sink connector '%s' topic must not be null", getName()));
+        this.topic = config.get(KafkaOptions.TOPICS);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public DataStreamSink<T> createSinkDataStream(DataStream<T> stream) {
         return Try.of(() -> {
+
             KafkaSerializationAdapter<T> serializationAdapter =
                     Optional.ofNullable((KafkaSerializationAdapter<T>) getSerializationAdapter())
                             .orElse(new KafkaSerializationAdapter<>(new DefaultSerializationSchema<>(), getOutputType()));
 
-            KafkaSinkBuilder<T> kafkaSinkBuilder = KafkaSink.<T>builder().setBootstrapServers(bootStrapServers);
-            KafkaRecordSerializationSchemaBuilder<T> serializerBuilder = KafkaRecordSerializationSchema.builder().setValueSerializationSchema(serializationAdapter.getSerializationSchema());
+            KafkaRecordSerializationSchemaBuilder<T> serializerBuilder =
+                    KafkaRecordSerializationSchema.builder().setValueSerializationSchema(serializationAdapter.getSerializationSchema());
+
+            KafkaRecordSerializationSchema<T> serializer =
+                    Optional.ofNullable(topic)
+                            .map(t -> serializerBuilder.setTopic(t).build())
+                            .orElseGet(() ->
+                                    serializerBuilder.setTopicSelector(Optional.ofNullable(serializationAdapter.getTopicSelector())
+                                            .orElseThrow(() -> new ConfigurationException(ErrCode.STREAMING_CONNECTOR_FAILED, ""))).build());
 
             KafkaSink<T> sink =
-                    topic != null ?
-                            kafkaSinkBuilder.setRecordSerializer(serializerBuilder.setTopic(topic).build()).build() :
-                            kafkaSinkBuilder.setRecordSerializer(serializerBuilder.setTopicSelector(serializationAdapter.getTopicSelector()).build()).build();
+                    KafkaSink.<T>builder()
+                            .setBootstrapServers(bootStrapServers)
+                            .setRecordSerializer(serializer)
+                            .build();
 
             return stream.sinkTo(sink);
-
         }).getOrElseThrow(e -> {
             if (e instanceof ClassCastException) {
                 return new FlinkException(
