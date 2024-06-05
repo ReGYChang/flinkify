69e9ea12d4895a95b38aae2fe6c7d0a407db6dc6
ReGYChang <p714140432@gmail.com>
2024-06-05 09:58:48 +0800

    build: prepare POM files for release by cleaning dependencies and formatting

---
+++ quantalink-quickstart/quantalink-quickstart-connector/quantalink-quickstart-connector-datagen/src/main/java/com/regy/quantalink/quickstart/connector/datagen/source/SimplePojo.java
@@ -0,0 +1,11 @@
+package com.regy.quantalink.quickstart.connector.datagen.source;
+
+import lombok.Data;
+
+@Data
+public class SimplePojo {
+
+    private String name;
+
+    private Integer age;
+}
